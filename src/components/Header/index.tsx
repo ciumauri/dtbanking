52db5608205ab5d54ed9cd19fc09943775c3e471
Mauricio Oliveira <ciumauri@gmail.com>
2022-11-04 10:09:15 -0300

    Feat:Refatorando nome da aplicação para dtbanking

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,20 @@
-import logoImg from "../../assets/logo.svg";
+import logoImg from '../../assets/logo.svg'
 
-import { Container, Content } from "./styles";
+import { Container, Content } from './styles'
 
 interface HeaderProps {
-  onOpenNewBetModal: () => void;
+  onOpenNewBetModal: () => void
 }
 
 export function Header({ onOpenNewBetModal }: HeaderProps) {
   return (
     <Container>
       <Content>
-        <img src={logoImg} alt="dt money" />
+        <img src={logoImg} alt="dt banking" />
         <button type="button" onClick={onOpenNewBetModal}>
           Nova entrada
         </button>
       </Content>
     </Container>
-  );
+  )
 }
